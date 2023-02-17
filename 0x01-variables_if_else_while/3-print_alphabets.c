17ef8041e5afa48ef8745961bb6706618daeece4
FatmaMostafa8 <122815612+FatmaMostafa8@users.noreply.github.com>
2023-02-18 01:14:37 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,18 +4,13 @@
  * Description: prints the alphabet in lowercase, and then in uppercase
  * Return: always 0
  */
-
 int main(void)
 {
 	char c;
 	for (c = 'a'; c <= 'z'; c++)
-	{
 		putchar(c);
-	}
 	for (c = 'A'; c <= 'Z'; c++)
-	{
 		putchar(c);
-	}
 	putchar('\n');
 	return (0);
 }
