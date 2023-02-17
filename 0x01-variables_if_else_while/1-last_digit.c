b397cba14bb386a11932da6b1433d634e106a9fd
fatma <famo0801@gmail.com>
2023-02-17 14:03:28 -0800

    Task 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /**
  * main - entry point
- * Description: check last digital of number n.
+ * Description: check last digit of number n.
  * Return: 0
  */
 int main(void)
@@ -12,19 +12,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	ld = n % 10;
-	printf("Last digital %d is ", n)
-	if (ld > 5)
+	last_digit = n % 10;
+	printf("Last digit %d is ", n)
+	if (last_digit > 5)
 	{
-		printf("%d and is greater than 5\n", ld);
+		printf("%d and is greater than 5\n", last_digit);
 	}
-	else if (ld == 0)
+	else if (last_digit == 0)
 	{
-		printf("%d and is 0\n", ld);
+		printf("%d and is 0\n", last_digit);
 	}
-	else if (ld < 6)
+	else if (last_digit < 6)
 	{
-		printf("%d and is less than 6 and not 0\n", ld);
+		printf("%d and is less than 6 and not 0\n", last_digit);
 	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Enter point
+ * Description: prints the alphabet in lowercase
+ * Return: always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
