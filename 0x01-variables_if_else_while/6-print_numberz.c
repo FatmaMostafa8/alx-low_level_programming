ed02ade18a648ac59fc7a84e9dad4cec4c20c598
FatmaMostafa8 <122815612+FatmaMostafa8@users.noreply.github.com>
2023-02-18 01:29:33 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,14 @@
 #include <stdio.h> 
-  
-/** 
- * main - Entry point* 
- * Return: Always 0 
- */ 
- 
+/**
+ * main - Entry point*
+ * Return: Always 0
+ */
 int main(void) 
-{ 
-        int i; 
+{
+        int i;
  
-        for (i = 0; i < 10; i++) 
-        putchar(i + '0'); 
-        putchar('\n'); 
-        return (0); 
+        for (i = 0; i < 10; i++)
+        putchar(i + '0');
+        putchar('\n');
+        return (0);
 }
