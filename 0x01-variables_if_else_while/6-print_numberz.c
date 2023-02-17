b459fed4c1b272647b0dada4e8432a44bd9d56ec
FatmaMostafa8 <122815612+FatmaMostafa8@users.noreply.github.com>
2023-02-18 01:41:37 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-/**
- * Desription: prints all single digit numbers of base 10 starting from 0
- * Return: 0
- */
+/*Desription: prints all single digit numbers of base 10 starting from 0*/
+/* Return: 0*/
 int main(void)
 {
 int n;
