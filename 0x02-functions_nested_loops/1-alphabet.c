2a5e6dec81f7889d6a64c98bc5b8f8d2d97207f0
FatmaMostafa8 <122815612+FatmaMostafa8@users.noreply.github.com>
2023-02-21 22:01:19 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,13 @@
 
 /**
  * main - check the code
- *
+ * print_alphabet prints the alphabet, in lowercase
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	char letter = 'a';
+	
 	while (letter <= 'z')
 	{
 		_putchar(letter);
